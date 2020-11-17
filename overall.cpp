17660b212accebd38fe6da45abd237cf29b14c5b
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-17 18:02:13 +0800

    Update overall.cpp

--- overall.cpp
+++ overall.cpp
@@ -12,7 +12,8 @@ int life = 100;
 bool lose;
 
 void show() {
-    usleep(5000);
+    cout << endl;
+    usleep(15000);
     vector<string>::iterator thing;
     cout << "---------------------------------------------------------------------------------------" << endl;
     cout << "        CP: " << life << "                    Bag: ";
@@ -38,7 +39,7 @@ void endGame() {
 }
 
 void castle() {
-    
+
 }
 
 void guessKey() {
@@ -63,11 +64,11 @@ void guessKey() {
 
     string userAnswer;
     int trial = 0;
-    bool correct = false;
+    bool correct;
 
     cout << endl;
 
-    while (!correct && trial < 3) {
+    while (!correct && trial != 3) {
         cout << endl;
         if (hereWord == "treasure") {
             if (trial == 0) {
@@ -191,12 +192,12 @@ void castleIn() {
     else {
         cout << "An elf jumps out and said, \"Sad for you, " << name << ". \n";
         cout << "Seems you got no key in hand. Do you want a last choice to get the key?\"" << endl;
-        cout << "Your response: Yes or No (y/n) " << endl;
+        cout << "Your response: (y: yes / n: no) " << endl;
         char answer;
         cin >> answer;
         while (answer != 'y' && answer != 'n') {
             cout << "Invalid Input! Please answer again.\n";
-            cout << "Your response: Yes or No (y/n) ";
+            cout << "Your response: (y: yes / n: no) ";
             cin >> answer;
         }
         if (answer == 'y')
@@ -206,10 +207,104 @@ void castleIn() {
     }
 }
 
+void defense() {
+    char answer;
+    cout << "Will you use the stick to defense and keep you safe, or do nothing?" << endl;
+    cout << "Your response: (y: defense / n: not defense)" << endl;
+    cin >> answer;
+    cout << endl;
+    bool canDefense;
+    vector<string>::iterator find;
+    while (answer != 'y' && answer != 'n') {
+        cout << "Invalid Input! Please answer again.\n";
+        cout << "Your response: (y: yes / n: no) ";
+        cin >> answer;
+    }
+    if (answer == 'y') {
+        for (find = bag.begin(); find != bag.end(); find++) {
+            if (*find == "stick") 
+                canDefense = true;
+        }
+    }
+    if (answer == 'n') {
+        canDefense = false;
+    }
+
+    if (canDefense) {
+        char cD[] = "You try your best to defense and protect yourself. \n"
+            "Although you lost your stick during the fight, you only can a little hurt. \n \n" 
+            "Your CP is decreased by 10. You keep your tired body to keep on the adventure";
+        for (int i = 0; i < sizeof(cD); i++) {
+            cout << cD[i];
+            usleep(3000);
+        }
+        life -= 10;
+    }
+    else {
+        char nD[] = "You choose not to fight with the wolf so you are seriously injuried. \n"
+        "Although you are hurt, you still insist to keep on your journey. \n \n" 
+        "Your CP is decreased by 30. You now keep on the adventure";
+        for (int k = 0; k < sizeof(nD); k++) {
+            cout << nD[k];
+            usleep(3000);
+        }
+        life -= 30;
+    }
+}
+
+void wolf() {
+    show();
+    cout << endl;
+    char word[] = "You walk in the forrest and you accidentally meet a wolf. \n"
+        "The wolf is scary and horrible He is full of hunger. ";
+        for (int i = 0; i < sizeof(word); i++) {
+            cout << word[i];
+            usleep(3000);
+        }
+    cout << endl;
+    char answer;
+    cout << "Do you have any food that can give him and avoid being eaten? (y: yes / n: no)" << endl;
+    cin >> answer;
+    cout << endl;
+    bool haveFood;
+    vector<string>::iterator find;
+    for (find = bag.begin(); find != bag.end(); find++) {
+        if (answer == 'y') {
+            if (*find == "steak" || *find == "fish")
+                haveFood = true;
+            else
+                haveFood = false;
+        }
+        else
+            haveFood = false;
+    }
+
+    if (haveFood) {
+        char food[] = "The wolf is happy and full now. \n"
+        "He walks away without hurting you and you are safe now. \n \n" 
+        "You can keep on your adventure.";
+        for (int j = 0; j < sizeof(food); j++) {
+            cout << food[j];
+            usleep(3000);
+        }
+    }
+    if (!haveFood) {
+        char nofood[] = "You have no food to give him!! \n"
+            "The wolf is very angry!!!!!! \n"
+            "He is going to attack you and you are thinking if you should defense or not \n \n";
+        for (int k = 0; k < sizeof(nofood); k++) {
+            cout << nofood[k];
+            usleep(3000);
+        }
+        defense();
+    }
+
+}
+
 void lake() {
     show();
     cout << endl;
-    char word[] = "You walk to the left side of the road and you see a lake in front of you. \n"
+    char word[] = "You walk to the right side of the road and you see a lake in front of you. \n"
         "You decide to take a rest next to the lake. \n"
         "When you are sitting down, your wood stick falls into the lake. \n" 
         "You have no idea what to do.\n"
@@ -223,7 +318,7 @@ void lake() {
     }
     cout << endl;
     char userChoice;
-    cout << "You decide to play: Rock, Paper or Scissors? (r/p/s)" << endl;
+    cout << "You decide to play: (r: rock / p: paper / s: scissors)? " << endl;
     bool userWin = false;
     bool again = false;
     char randChoice[3] = {'r','p','s'};
@@ -234,14 +329,14 @@ void lake() {
     cout << endl;
     
     while (userChoice != 'r' && userChoice != 'p' && userChoice != 's') {
-        cout << "Invalid Input! Play again. Rock, Paper or Scissors? (r/p/s)" << endl;
+        cout << "Invalid Input! Play again. (r: rock / p: paper / s: scissors)? " << endl;
         fairyDecision = randChoice[rand() % 3];
         cin >> userChoice;
     }
 
     while (fairyDecision == userChoice) {
         cout << "Fairy plays " << fairyDecision << endl;
-        cout << "Draw. Play again. Rock, Paper or Scissors? (r/p/s)" << endl;
+        cout << "Draw. Play again.  (r: rock / p: paper / s: scissors)? " << endl;
         fairyDecision = randChoice[rand() % 3];
         cin >> userChoice;
     }
@@ -279,18 +374,52 @@ void lake() {
         bag.pop_back();
     }
     cout << endl;
-    castleIn();
+    wolf();
 }
 
 void woodhouse() {
     show();
     cout << endl;
-}
-
-void wolf() {
-    show();
+    char word[] = "You walk to the left side of the road and you see a wooden hut in front of you. \n"
+        "You would like to go inside and take a rest. \n" 
+        "When you get in there, you see a poor little boy crying with no reason.";
+        for (int i = 0; i < sizeof(word); i++) {
+            cout << word[i];
+            usleep(3000);
+        }
+    cout << endl;
+    char answer;
+    cout << "Would you comfort him and see if anything can help? (y: yes / n: no)" << endl;
+    cin >> answer;
     cout << endl;
 
+    while (answer != 'y' && answer != 'n') {
+        cout << "Invalid Input! Please answer again.\n";
+        cout << "Your response: (y: yes / n: no) ";
+        cin >> answer;
+    }
+    if (answer == 'y') {
+        char comfort[] = "The kid cried because his robot is broken. \n"
+        "He feels better now after your comfort. \n" 
+        "He is very happy so he gives you a steak to thanks for your compassion. \n \n"
+        "You receive a steak and keep on your adventure.";
+        for (int j = 0; j < sizeof(comfort); j++) {
+            cout << comfort[j];
+            usleep(3000);
+        }
+        bag.push_back("steak");
+    }
+    if (answer == 'n') {
+        char notcomfort[] = "The kid keeps crying so you cannot take a nap. \n"
+        "You are bothered and you are not energetic as before anymore. \n \n" 
+        "Your CP is decreased by 20. You keep your tired body to keep on the adventure";
+        for (int k = 0; k < sizeof(notcomfort); k++) {
+            cout << notcomfort[k];
+            usleep(3000);
+        }
+        life -= 20;
+    }
+    wolf();
 }
 
 void tree() {
@@ -309,7 +438,7 @@ void red() {
 void folk() {
     char input;
     char word[] = "You just walk into the forrest and meet your first folk in the road (literally).\n"
-        "Now, go left or right? (l/r) ";
+        "Now, go left or right (l: left / r: right)? ";
     for (int i = 0; i < sizeof(word); i++) {
         cout << word[i];
         usleep(3000);
@@ -318,7 +447,7 @@ void folk() {
     cout << endl;
     while (input != 'l' && input != 'r') {
         cout << "Invalid Input! Please answer again.\n";
-        cout << "Go left or right? (l/r) ";
+        cout << "Go left or right (l: left / r: right)? ";
         cin >> input;
     }
     if (input == 'l')
@@ -407,6 +536,8 @@ void information() {
 
 int main() {
     system("clear");
+    if (life <= 0)
+        lose = true;
     while (!lose)
         information();
 
