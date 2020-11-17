90038e0abccebdb0060436c3c3861328a4a97b56
makkaho <71927666+makkaho@users.noreply.github.com>
2020-11-17 18:41:11 +0800

    Create monsterplayer.cpp

---
+++ monsterplayer.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+struct monsterposition{
+    int minx;
+    int miny;
+    int maxx;
+    int maxy;
+}
+
+monsterposition mp;
+
+struct ballotposition{
+    int mina;
+    int minb;
+    int maxa;
+    int maxb;
+}
+
+ballotposition bp;
+
+
+int monsterposition(&mp.minx, &mp.miny, &mp.maxx, &mp.maxy)
+{
+    int sizeofmonster=10;
+    srand(time(NULL));
+    int a=rand()%100, b=rand()%100;
+   mp.minx=a;
+   mp.miny=b;
+   mp.maxx=a+sizeofmonster;
+   mp.miny=b+sizeofmonster;
+   return 0;
+}
+
+int ballotposition(&bp.mina, &bp.minb, &bp.maxa, &bp.maxb,&c,&d){
+    int sizeofballot=1;
+    srand(time(NULL));
+   bp.mina=c;
+   bp.minb=d;
+   bp.maxa=c+sizeofmonster;
+   bp.minb=d+sizeofmonster;
+   return 0;
+}
