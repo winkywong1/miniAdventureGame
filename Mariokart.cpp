6098888c59605dd43dfd58f908bc5f17f26f691f
makkaho <71927666+makkaho@users.noreply.github.com>
2020-11-18 22:44:50 +0800

    Create Mariokart.cpp

---
+++ Mariokart.cpp
@@ -0,0 +1,182 @@
+# include <iostream>
+# include <unistd.h>
+# include <cstdio>
+
+using namespace std;
+
+bool gameover(){
+  return false;
+}
+
+string jump(){
+  //system("clear");
+  for (int i=0;i<=3;i++){
+    cout<<" "<<endl;
+  }
+  cout<<"                                                   "<<"ooooo"<<endl;
+  for (int i=0;i<=2;i++){
+    cout<<" "<<endl;
+  }
+}
+
+int main(){
+  while (!gameover()){
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"  ####################  ####################  ####################  ####################"<<endl;
+    usleep(100000);
+    system("clear");
+    char x=getchar();
+    if (x=='a'){jump();}
+    else{
+      for (int i=0;i<=5;i++){
+        cout<<" "<<endl;
+      }
+      cout<<"                                                   "<<"ooooo"<<endl;
+    }
+      cout<<"#  ####################  ####################  ####################  ###################"<<endl;
+      usleep(100000);
+
+
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"##  ####################  ####################  ####################  ##################"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"###  ####################  ####################  ####################  #################"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"####  ####################  ####################  ####################  ################"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"#####  ####################  ####################  ####################  ###############"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"######  ####################  ####################  ####################  ##############"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"#######  ####################  ####################  ####################  #############"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"########  ####################  ####################  ####################  ############"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"#########  ####################  ####################  ####################  ###########"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"##########  ####################  ####################  ####################  ##########"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"###########  ####################  ####################  ####################  #########"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"############  ####################  ####################  ####################  ########"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"#############  ####################  ####################  ####################  #######"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"##############  ####################  ####################  ####################  ######"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"###############  ####################  ####################  ####################  #####"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"################  ####################  ####################  ####################  ####"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"#################  ####################  ####################  ####################  ###"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"##################  ####################  ####################  ####################  ##"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"###################  ####################  ####################  ####################  #"<<endl;
+    usleep(100000);
+    system("clear");
+    for (int i=0;i<=5;i++){
+      cout<<" "<<endl;
+    }
+    cout<<"                                                   "<<"ooooo"<<endl;
+    cout<<"####################  ####################  ####################  ####################  "<<endl;
+    usleep(100000);
+    system("clear");
+  }
+
+
+
+
+}
