ff78e615c9741dbab4cf71fb0d02729e5fddc307
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-19 03:12:44 +0800

    Update monsterplayer.cpp

--- monsterplayer.cpp
+++ monsterplayer.cpp
@@ -21,24 +21,24 @@ struct ballotposition{
 ballotposition bp;
 
 
-int monsterpos(int &p, int &q, int &r, int &s)
+void monsterpos(int &p, int &q, int &r, int &s)
 {
     int sizeofmonster=90;
     srand(time(NULL));
     int a=rand()%100, b=rand()%100;
-   p=a;
-   q=b;
-   r=a+sizeofmonster;
-   s=b+sizeofmonster;
-   return 0;
+    p=a;
+    q=b;
+    r=a+sizeofmonster;
+    s=b+sizeofmonster;
+    return 0;
 }
 
 int ballotpos(int &p,int &q,int &r,int &s,int &c,int &d){
     int sizeofballot=30;
-   p=c;
-   q=d;
-   r=c+sizeofballot;
-   s=d+sizeofballot;
+    p=c;
+    q=d;
+    r=c+sizeofballot;
+    s=d+sizeofballot;
 }
 
 bool IsOverlap(monsterposition p, ballotposition q) {
@@ -50,22 +50,28 @@ bool IsOverlap(monsterposition p, ballotposition q) {
   }
 }
 
-int shoot(){
-  cout<<"Now a monster bird is in front of you. This monster bird is invisible but its body is big. "<<endl;
-  cout<<"You have a cannon and you can use it to shoot to any position"<<endl;
-  cout<<"The monster is 90cm x 90cm while your ballot is 50cm x 50cm"<<endl;
-  cout<<"Imagine there is a grid in front of you with x-axis (0-100) and y-axis (0-100)"<<endl;
-  cout<<"Please input a pair of numbers indicating x-axis and y-axis to show you location of your shooting target"<<endl;
-  int c,d;
-  cin>>c>>d;
-  monsterpos(mp.minx, mp.miny, mp.maxx, mp.maxy);
-  ballotpos(bp.mina, bp.minb, bp.maxa, bp.maxb,c,d);
-  if (IsOverlap(mp,bp)){
-    cout<<"You win! "<<endl;
-  }
+void shoot(){
+    cout<<"Now a monster bird is in front of you. This monster bird is invisible but its body is big. "<<endl;
+    cout<<"You have a cannon and you can use it to shoot to any position"<<endl;
+    cout<<"The monster is 90cm x 90cm while your ballot is 50cm x 50cm"<<endl;
+    cout<<"Imagine there is a grid in front of you with x-axis (0-100) and y-axis (0-100)"<<endl;
+    cout<<"Please input a pair of numbers indicating x-axis and y-axis to show you location of your shooting target"<<endl;
+    int c,d;
+    cout << "x-axis: ";
+    cin >> c;
+    cout << endl << "y-axis: ";
+    cin >> d;
+    monsterpos(mp.minx, mp.miny, mp.maxx, mp.maxy);
+    ballotpos(bp.mina, bp.minb, bp.maxa, bp.maxb,c,d);
+    if (IsOverlap(mp,bp))
+        cout<<"You win! "<<endl;
+
   while (!IsOverlap(mp,bp)){
-    cout<<"You lose. Please try again."<<endl;
-    cin>>c>>d;
+    cout<<"You fail to hit the monster! Please try again."<<endl;
+    cout << "x-axis: ";
+    cin >> c;
+    cout << endl << "y-axis: ";
+    cin >> d;
     monsterpos(mp.minx, mp.miny, mp.maxx, mp.maxy);
     ballotpos(bp.mina, bp.minb, bp.maxa, bp.maxb,c,d);
     if (IsOverlap(mp,bp)){
