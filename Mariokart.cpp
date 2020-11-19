c9ad8170de84677406149fee9f942f2161606e18
makkaho <71927666+makkaho@users.noreply.github.com>
2020-11-19 13:03:10 +0800

    Update Mariokart.cpp

--- Mariokart.cpp
+++ Mariokart.cpp
@@ -4,8 +4,13 @@
 
 using namespace std;
 
-bool gameover(){
-  return false;
+bool gameover(int i){
+  if (i>5){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 string jump(){
@@ -19,9 +24,18 @@ string jump(){
   }
 }
 
-int main(){
-  int count;
-  while (!gameover()){
+int run(){
+  cout<<"Welcome to the volcano land. This is a wonderful land but it has 5 holes. Your mission is to pass all the holes.";
+  cout<<"If you want to jump, press certain times of 'a' and then press Enter"<<endl;
+  cout<<"YOu just need to make the decision on how many jump you will have"<<endl;
+  cout<<"If you fail to jump in anyone of the 5 holes, you will lose the game"<<endl;
+  cout<<"You need to make the jumping decision before you start running"<<endl;
+  cout<<"For example, if you want to jump for 3 times during the running, you just need to press aaa and the press Enter before you start running"<<endl;
+  cout<<"For example, if you press aabaa, you will lose the game"<<endl;
+  cout<<"Ready? The game will start soon"<<endl;
+  usleep(30000000);
+  int count=0;
+  while (!gameover(count)){
     for (int i=0;i<=5;i++){
       cout<<" "<<endl;
     }
@@ -102,6 +116,8 @@ int main(){
       cout<<"                                                   "<<"ooooo"<<endl;
       cout<<"########  ####################  ####################  ####################  ############"<<endl;
       cout<<"gameOver"<<endl;
+      cout<<"You have already dropped into the hole."<<endl;
+      break;
       usleep(1000000);
     }
 
@@ -201,9 +217,14 @@ int main(){
     usleep(100000);
     system("clear");
     count++;
+
+  if (count>5){
+    cout<<"You have passed all the hole"<<endl;
   }
 
+  }
 
-
-
+}
+int main(){
+  run();
 }
