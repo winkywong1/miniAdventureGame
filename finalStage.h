754503886a3e14510d13c4b140edbaee0b7fa9aa
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-21 04:50:50 +0800

    Update finalStage.h

--- finalStage.h
+++ finalStage.h
@@ -2,8 +2,10 @@
 #ifndef FINALSTAGE_H
 #define FINALSTAGE_H
 
+const int maxNumOfPoker = 5;
+
 struct player;
-void calBlackjack();
+void calBlackjack(int num, char number[maxNumOfPoker], char a, int *mina, int *maxa, int *minto, int *maxto);
 void blackjack();
 void finalStage();
 
