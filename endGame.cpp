622cb9cdc823e43eeed23ebf67fe723eb6ec8f7e
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-21 14:16:27 +0800

    Update endGame.cpp

--- endGame.cpp
+++ endGame.cpp
@@ -9,15 +9,20 @@ int life = 100;
 
 // when the player fails to pass all the tasks or his/her CP reaches equal or less than 0, this printout will be shown to indicate the failure of game.
 // the game will end right after printing the printout
+// the printout of end game will be shown by reading input from the text file "endGameMsg.txt"
 // the information including the name, failure and remain value of CP will be stored in a file "result.txt"
 void endGame() {
-    cout << endl << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
-    cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl << endl;;
-    cout << "                              You fail to save your dog.                               " << endl;
-    usleep(5000);
-    cout << endl << "                  You can never see your dog and you are very upset.                   " << endl;
-    usleep(5000);
-    cout << endl << "                             - - G A M E     O V E R - -" << endl;
+    ifstream fin;
+    fin.open("endGameMsg.txt");
+    if (fin.fail()){
+        cout<<"error in file opening"<<endl;
+        exit(1);
+    }
+    string msg;
+    while (getline(fin,msg)){
+        cout << msg << endl;
+    }
+    fin.close();
 
 	ofstream fout;
     fout.open("result.txt", ios::app);
@@ -30,3 +35,4 @@ void endGame() {
     fout.close();
     exit(0);
 }
+
