4cc485d16d025548a07a9c845c4a508dc5222682
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-21 14:24:03 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,38 +1,14 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
-#include "endGame.h"
 #include "start.h"
+#include "stat.h"
 
 using namespace std;
-string name;
-int life = 100;
-bool lose;
 
 int main() {
-	ofstream fout;
-    fout.open("result.txt", ios::app);
-
-    if ( fout.fail() ) {
-        cout << "Error in file opening!" << endl;
-        exit(1);
-    }
-
     system("clear");
-
-    if (life <= 0) {
-        lose = true;
-        endGame();
-    }
-    while (!lose)
-        information();
-
-    fout << "Name" << ": " << name << endl;
-    if (lose)
-        fout << "Result: " << "lose the game with " << life << " CP left." << endl;
-    if (!lose)
-        fout << "Result: " << "win the game with " << life << " CP left." << endl;
-    fout.close();
+    information();
     
     return 0;
 }
