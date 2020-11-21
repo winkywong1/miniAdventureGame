f216b66b37ef1578d72e6970a080124f102a5086
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-21 12:34:45 +0800

    Update bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -1,97 +1,86 @@
 # include <iostream>
 # include <fstream>
+# include <unistd.h>
 using namespace std;
 
-int bingo(){
-  ifstream fin;
-  fin.open("bingo.txt");
-  if (fin.fail()){
-    cout<<"error in file opening"<<endl;
-    exit(1);
-  }
-  string msg;
-  while (getline(fin,msg)){
-    cout<<msg<<endl;
-  }
-  fin.close();
-  int card[25],user[25];
-  srand(time(NULL));
-  for (int i=0;i<25;i++){
-    int a=rand()%50;
-    card[i]=a;
-  }
+int life = 100;
+void bingo(){
+    cout << endl;
+    char word[] = "You are tired and you reach the restaurant. \n"
+        "In this restaurant, you will have the chance to replenish your CP value. \n"
+        "You will play a game and your performance of this game will determine the gain in CP value. \n \n" 
+        "There are 25 numbers on the card, arranging in a 5 x 4 manner. \n"
+        "The number should be between 1 and 100. \n"
+        "The waitress is holding the 20 cards and plays Bingo with you. \n"
+        "You have to write 5 groups of number, each group contains 2 numbers. \n"
+        "That means there are total 10 numbers inputted by you. \n"
+        "We will calculate the sum and difference of each group of numbers.  \n"
+        "After inputing the number, each group of number will be displayed as \n"
+        "\"1st input\", \"2nd input\", \"Sum of two inputs\", and \"difference of two inputs\" \n"
+        "If the number on your card matches the number on the waitress's, 5 CP will be rewarded.  \n";
+    for (int i = 0; i < sizeof(word); i++) {
+        cout << word[i];
+        usleep(3000);
+    }
 
-  int input1, input2;
-  int sum, diff, prod, quo;
-  for (int i=0;i<5;i++){
-    cin>>input1>>input2;
-    sum=input1+input2;
-    prod=input1*input2;
-    if (input1>input2){
-      diff=input1-input2;
+    int card[25],user[25];
+    srand(time(NULL));
+    for (int i = 0; i < 20; i++){
+      int a = rand() % 50 + 1;
+      card[i] = a;
     }
-    else {
-      diff=input2-input1;
-    }
-    if (input1>input2){
-      quo=input1/input2;
-    }
-    else {
-      quo=input2/input1;
-    }
-    user[5*i]=sum;
-    user[5*i+1]=diff;
-    user[5*i+2]=input1;
-    user[5*i+3]=input2;
-    user[5*i+4]=sum+diff;
-  }
 
-  cout<<"This is the number on our card"<<endl;
-  for (int i=0;i<25;i++){
-    if (i==4 || i==9 || i==14 || i==19 || i==24){
-      cout<<card[i]<<endl;
+    int input1, input2, sum, diff;
+    for (int i=0;i<5;i++){
+        cout << endl << "\"1st input\" & \"2nd input\" of the " << i+1 << " Group: ";
+        cin >> input1 >> input2;
+        sum = input1 + input2;
+        if (input1 > input2){
+            diff = input1 - input2;
+        }
+        else {
+            diff = input2 - input1;
+        }
+        user[4*i] = input1;
+        user[4*i+1] = input2;
+        user[4*i+2] = sum;
+        user[4*i+3] = diff;
     }
-    else{
-      cout<<card[i]<<" ";
+    
+    cout << endl << "This is the number on our card" << endl;
+    for (int i=0;i<20;i++){
+        if (i == 3 || i == 7 || i == 11 || i == 15 || i == 19){
+            cout<<card[i]<<endl;
+        }
+        else{
+            cout<<card[i]<<" ";
+        }
     }
-  }
 
-  cout<<endl<<endl;
+    cout<<endl<<endl;
 
-  cout<<"This is the number on your card"<<endl;
-  for (int i=0;i<25;i++){
-    if (i==4 || i==9 || i==14 || i==19 || i==24){
-      cout<<user[i]<<endl;
+    int count = 0;
+
+    cout<<"This is the number on your card"<<endl;
+    for (int i=0;i<20;i++){
+        if (i == 3 || i == 7 || i == 11 || i == 15 || i == 19){
+            cout<<user[i]<<endl;
+        }
+        else{
+            cout<<user[i]<<" ";
+        }
     }
-    else{
-      cout<<user[i]<<" ";
+
+    cout << endl;
+    for (int i=0;i<20;i++){
+        for (int j=0;j<20;j++){
+            if (card[i]==user[j]){
+                count += 1;
+            }
+        }
     }
-  }
-
-  cout<<endl<<endl;
-  char x='x';
-  for (int i=0;i<25;i++){
-    for (int j=0;j<25;j++){
-      if (card[i]==user[j]){
-        card[i]=0;
-      }
-    }
-  }
-
-  cout<<"This is the number of our card after comparing with your card"<<endl;
-  for (int i=0;i<25;i++){
-    if (i==4 || i==9 || i==14 || i==19 || i==24){
-      cout<<card[i]<<endl;
-    }
-    else{
-      cout<<card[i]<<" ";
-      life+=5;
-    }
-  }
-
-
-}
-
-int main(){
-  bingo();
+    int bonus = count*5;
+    life += bonus;
+    cout << endl << "There are in total " << count << " match" << endl;
+    cout << bonus << " bonus of CP will be rewarded." << endl;
 }
