3574a471ecfc232a5ea3483fea68bef94bf0d6f9
winkywong1 <73123093+winkywong1@users.noreply.github.com>
2020-11-21 14:35:51 +0800

    Create stat.h

---
+++ stat.h
@@ -0,0 +1,15 @@
+// stat.h
+#ifndef STAT_H
+#define STAT_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+string name;
+vector<string> bag;
+int life = 100;
+bool lose;
+int chance = 0;
+
+#endif
